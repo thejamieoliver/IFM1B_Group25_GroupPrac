18192244c0531c66f3335636c72d363a456f4300
JAMIE OLIVER <220015262@student.uj.ac.za>
2020-09-30 11:58:29 +0200

    Add project files.

---
+++ IFM1B_Group25_GroupPrac/Form1.vb
@@ -0,0 +1,15 @@
+﻿Option Strict On
+Option Explicit On
+Option Infer Off
+' *****************************************************************
+' Team Number: assigned to team
+' Team Member 1 Details: Surname, Initials (Student #)
+' Team Member 2 Details: Surname, Initials (Student #)
+' Team Member 3 Details: Surname, Initials (Student #)
+' Team Member 4 Details: e.g. Smith, J (202000001)
+' Practical: Team Project
+' Class name: (name of the class)
+' *****************************************************************
+Public Class Form1
+
+End Class
