0039aa54999cdea9448b21a1185f522a88ca5839
AndogZA <52880870+AndogZA@users.noreply.github.com>
2020-09-30 12:50:50 +0200

    Added details to Form1

--- IFM1B_Group25_GroupPrac/Form1.vb
+++ IFM1B_Group25_GroupPrac/Form1.vb
@@ -2,13 +2,13 @@
 Option Explicit On
 Option Infer Off
 ' *****************************************************************
-' Team Number: assigned to team
-' Team Member 1 Details: Surname, Initials (Student #)
-' Team Member 2 Details: Surname, Initials (Student #)
-' Team Member 3 Details: Surname, Initials (Student #)
-' Team Member 4 Details: e.g. Smith, J (202000001)
+' Team Number: 25
+' Team Member 1 Details: Crossling, CJ (220108433)
+' Team Member 2 Details: Milne, R (220036931)
+' Team Member 3 Details: Ogden, AR (220078628)
+' Team Member 4 Details: Oliver, J (220015262)
 ' Practical: Team Project
-' Class name: (name of the class)
+' Class name: Form1
 ' *****************************************************************
 Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
