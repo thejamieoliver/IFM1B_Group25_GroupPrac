4e2b52e9cfa95c619a9090aa1a466f0e54b1301f
RICHARD MILNE <220036931@student.uj.ac.za>
2020-10-06 15:38:00 +0200

    Added code

--- cHIV/cHIV.vb
+++ cHIV/cHIV.vb
@@ -1,3 +1,18 @@
-﻿Public Class cHIV
-    'Cleveland is excited about mongolian throat singing
+﻿Option Strict On
+Option Explicit On
+Option Infer Off
+' *****************************************************************
+' Team Number: 25
+' Team Member 1 Details: Crossling, CJ (220108433)
+' Team Member 2 Details: Milne, R (220036931)
+' Team Member 3 Details: Ogden, AR (220078628)
+' Team Member 4 Details: Oliver, J (220015262)
+' Practical: Team Project
+' Class name: cHIV
+' *****************************************************************
+Public Class cHIV
+
+    Inherits cDisease
+    Private _bloodCount As Double
+    'This is a change I made while testing with cleveland
 End Class
