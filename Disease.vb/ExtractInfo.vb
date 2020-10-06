187478995ae62dc9a231f715fd20996a2d29943e
CLEVELAND JAMES CROSSLING <220108433@student.uj.ac.za>
2020-10-06 15:21:45 +0200

    Added interface class and utility function to cDisease

---
+++ Disease.vb/ExtractInfo.vb
@@ -0,0 +1,9 @@
+﻿Public Interface ExtractInfo
+    Property DiseaseName As String
+    Property InfectionRate As Double
+    Property RecoveryRate As Double
+    Property NumSymptoms As Integer
+    Property TotalCases As Integer
+    Property TotalDeaths As Integer
+    Property TotalRecovers As Integer
+End Interface
